4c56297af91485f328a5bc170078450c63ce581c
BigMikeTC <dj.buckwild12@gmail.com>
2026-07-07 21:43:42 +0100

    setup eslint, prettier and Design Specs

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,24 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import prettier from 'eslint-config-prettier'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default tseslint.config(
+  { ignores: ['dist'] },
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
+      ...tseslint.configs.recommended,
+      reactHooks.configs.flat['recommended-latest'],
       reactRefresh.configs.vite,
+      prettier,
     ],
     languageOptions: {
+      ecmaVersion: 2023,
       globals: globals.browser,
     },
   },
-])
+)
